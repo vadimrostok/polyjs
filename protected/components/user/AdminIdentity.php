64884da3311f3984f5c86915165513277de84bcf
vadimrostok <vadimrostok@gmail.com>
2013-04-22 20:18:21 +0300

    fix db

--- protected/components/user/AdminIdentity.php
+++ protected/components/user/AdminIdentity.php
@@ -2,20 +2,20 @@
 
 class AdminIdentity extends CUserIdentity
 {
-	public function authenticate()
-	{
-		$db = Yii::app()->db;
-		$user = $db->createCommand('SELECT * FROM `admin` WHERE `login`=:username')
-			->bindParam(':username', $this->username)
-			->query()
-			->read();
-		if(!isset($user)) {
-			$this->errorCode = self::ERROR_USERNAME_INVALID;
-		} elseif(md5($user['salt'] . $this->password) !== $user['password']) {
-			$this->errorCode = self::ERROR_PASSWORD_INVALID;
-		} else {
-			$this->errorCode = self::ERROR_NONE;
-		}
-		return !$this->errorCode;
-	}
+    public function authenticate()
+    {
+        $db = Yii::app()->db;
+        $user = $db->createCommand('SELECT * FROM `admin` WHERE `login`=:username')
+            ->bindParam(':username', $this->username)
+            ->query()
+            ->read();
+        if(!isset($user)) {
+            $this->errorCode = self::ERROR_USERNAME_INVALID;
+        } elseif(md5($user['salt'] . $this->password) !== $user['password']) {
+            $this->errorCode = self::ERROR_PASSWORD_INVALID;
+        } else {
+            $this->errorCode = self::ERROR_NONE;
+        }
+        return !$this->errorCode;
+    }
 }
