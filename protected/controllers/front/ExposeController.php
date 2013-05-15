1de330a0dacf24fec8821776cb1fc68fb9dfc421
vadimrostok <vadimrostok@gmail.com>
2013-05-15 19:01:47 +0300

    visual updates

--- protected/controllers/front/ExposeController.php
+++ protected/controllers/front/ExposeController.php
@@ -24,7 +24,7 @@ class ExposeController extends Controller
             throw new CHttpException(404, 'No such data Shepi could found.');
         }
         $this->pageTitle = $albumModel->title;
-        $this->pageDescription = "\"" . $albumModel->title . "\"[*]~" . date('d.m.y', strtotime($albumModel->created_at)) . '. Shepi.';
+        $this->pageDescription = $albumModel->title . '[*]~' . date('d.m.y', strtotime($albumModel->created_at)) . '. Shepi.';
         $this->currentAlbum = $albumModel;
         $this->render(
             'list'/*, 
@@ -46,7 +46,7 @@ class ExposeController extends Controller
             throw new CHttpException(404, 'No such data Shepi could found.');
         }
         $this->pageTitle = $albumModel->title;
-        $this->pageDescription = "\"" . $albumModel->title . "\""
+        $this->pageDescription = $albumModel->title
          . '[' . $pictureModel->id . ']~' . date('d.m.y', strtotime($pictureModel->created_at)) . '. Shepi.';
         $this->currentAlbum = $albumModel;
         $this->currentPicture = $pictureModel;
